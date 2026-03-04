72df0347efcc625a04627e6a8ae0ad117230557f
NanditaReji-STUDENT <g00463584@atu.ie>
2026-03-04 16:14:24 +0000

    Initial commit to master

---
+++ src/main/java/com/example/oop_inclass_07/OopInclass07Application.java
@@ -0,0 +1,13 @@
+package com.example.oop_inclass_07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopInclass07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopInclass07Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oop_inclass_07/OopInclass07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oop_inclass_07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopInclass07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
