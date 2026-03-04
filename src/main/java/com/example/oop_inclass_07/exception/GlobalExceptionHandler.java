37d9fc2c244ac45205bf78f8e2fd2ab310898fac
NanditaReji-STUDENT <g00463584@atu.ie>
2026-03-04 17:05:49 +0000

    Added code under GlobalExceptionalHandler

--- src/main/java/com/example/oop_inclass_07/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/oop_inclass_07/exception/GlobalExceptionHandler.java
@@ -1,4 +1,27 @@
 package com.example.oop_inclass_07.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            String fieldName = fieldError.getField();
+            String errorMessage = fieldError.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 }
